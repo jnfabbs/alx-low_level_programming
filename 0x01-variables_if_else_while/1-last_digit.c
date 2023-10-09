ceb8a82c1e887096ea057f13975ce2cef75f8b28
John Fabian <johnfabian505@gmail.com>
2023-10-08 21:24:21 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+/**
+ * main - Entry point
+ * Description: checks on the last digit of the assigned var
+ * Return: Always 0 (success)
+ */
 
 int main(void)
 {
