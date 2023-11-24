e0391289067b136728ab8edda654300dff30e1be
John Fabian <johnfabian505@gmail.com>
2023-11-24 10:35:37 -0800

    files

---
+++ 0x13-more_singly_linked_lists/0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+ #include "main.h"
+
+ /**
+  * print alphabet - funtion of task
+  * Return: return 0
+  * /
+
+ void print_alphabet(void);
+ {
+
+ char c;
+ for(c = 'a'; c <= 'z'; c++)
+ {
+ _putchar(c);
+ }
+ _putchar('\n');
+ }
